2f07d67d65544b00a10f48e55238c6da133c6033
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-04-22 19:51:43 +0530

    studied sessions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const UserModel=require('./users');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+}); 
+
+/* router.get('/', function(req,res, next){
+  req.session.sessionname = "testsession",
+  res.render('index');
 });
+
+router.get('/check',function(req,res){
+  console.log(req.session);
+  res.send('session is working');
+})
+ */
+
 
 module.exports = router;
