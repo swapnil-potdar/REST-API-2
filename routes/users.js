285e9ca6acf386b26007cc296ee39f7c502494a2
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-04-25 22:41:26 +0530

    trying to comnnect to mongodb atlas

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,14 @@ const mongoose = require("mongoose");
 
 /* mongoose.connect("mongodb:127.0.0.1:27017/dbname"); */
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-  .then(() => console.log('Connected!'));
+/* mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!')); */
+
+  mongoose.connect(' mongodb+srv://potdarswapnil11:ASpmZsyla3kiDX0p@swapnil.zjqy3sn.mongodb.net/REST-API?retryWrites=true&w=majority')
+  .then(() => console.log('Connected to database!'));
+ 
+
+
 
 const userschema = mongoose.Schema({
   username: String,
